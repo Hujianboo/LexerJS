724a051d50942bc3cf6480db6f398ab4c513c59c
hujianbo <466636709@qq.com>
2020-10-01 21:25:41 +0800

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+class Token {
+  constructor(type,value){
+    this.type = type;
+    this.value = value;
+  }
+}
+class Interpreter {
+  constructor(input){
+    this.str = input;
+    this.pos = 0;
+  };
+  get_next_token(){
+    
+  }
+}
+let t = new Token('Interger',3);
